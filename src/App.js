9ca9600fd9a9ebaa6f06be465603a708bef73d52
Kunho Lee <gunho1020@gmail.com>
2018-04-11 23:42:33 +0900

    #4 TypographJS and Building for Production

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import styles from './styles.scss';
 
-export default () => <div className={styles.title}>Hello World React with Parcel!</div>;
+export default () => <div className={styles.title}>
+  <h1>Hello World React with Parcel!</h1>
+  <p>It's a very cool tool!</p>  
+</div>;
